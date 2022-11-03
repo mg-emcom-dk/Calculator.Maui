5d91b07ea9ffb0782674a34417052837a79f3c6f
mg-emcom-dk <55033278+mg-emcom-dk@users.noreply.github.com>
2022-11-04 00:18:08 +0100

    Add project files.

---
+++ SimpleCalculator/Business/Calculation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.Business;
+
+public static class Calculation
+{
+    public static double Calculate(string operation, double firstNumber, double secondNumber)
+    {
+        double result = 0;
+        switch (operation)
+        {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                if (secondNumber != 0)
+                {
+                    result = firstNumber / secondNumber;
+                }
+                else
+                {
+                    result = 0;
+                }                
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+}
---
+++ SimpleCalculator/MainPage.xaml.cs
@@ -0,0 +1,167 @@
+﻿using SimpleCalculator.Business;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace SimpleCalculator;
+
+public partial class MainPage : ContentPage
+{
+	int currentState = 1;
+	string operant;
+	double firstNumber;
+	double secondNumber;
+	int negativeNumber = 1;
+
+	//history
+	string currentOperationHistory;
+	List<string> history = new();
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+		OnClear(this, null);
+	}
+
+	void OnClear(object sender, EventArgs e)
+	{
+		firstNumber = 0;
+		secondNumber = 0;
+		currentState = 1;
+		this.result.Text = "0";
+		UpdateHistory(sender, e, "");
+	}
+
+	void OnSquared(object sender, EventArgs e)
+	{
+		if (firstNumber == 0)
+		{
+			return;
+		}
+
+        //history
+        currentOperationHistory = $"{firstNumber} * {firstNumber}";
+        UpdateHistory(sender, e, currentOperationHistory);
+
+        firstNumber = firstNumber * firstNumber;
+		this.result.Text = firstNumber.ToString();
+	}
+
+	void OnSquareRoot(object sender, EventArgs e)
+	{
+        if (firstNumber == 0)
+        {
+            return;
+        }
+
+        //history
+        currentOperationHistory = $"sqrt {firstNumber}";
+        UpdateHistory(sender, e, currentOperationHistory);
+
+        firstNumber = Math.Sqrt(firstNumber);
+        this.result.Text = firstNumber.ToString();        
+    }
+
+	void OnNumberSelection(object sender, EventArgs e)
+	{
+		Button button = (Button) sender;
+		string btnPressed = button.Text;
+        double number;
+
+        if (this.result.Text =="0" || currentState < 0)
+		{
+			this.result.Text = string.Empty;
+			if (currentState < 0) 
+			{ 
+				currentState *= -1;	
+			}
+		}
+
+        this.result.Text += btnPressed;				
+        
+		if (double.TryParse(this.result.Text, out number))
+		{
+            this.result.Text = number.ToString("N0");
+            if (currentState == 1) 
+			{ 
+				firstNumber = number;                
+            }
+            else 
+			{ 
+				secondNumber = number;                
+            }
+		}		
+	}
+    
+	void OnNegativeNumber(object sender, EventArgs e)
+	{
+		if (currentState == 1)
+		{
+			negativeNumber = -1;
+		}
+		else
+		{
+            negativeNumber = 1;
+        }
+		OnOperantSelection(sender, e);
+	}
+
+    void OnOperantSelection(object sender, EventArgs e)
+	{
+		
+		Button button = (Button) sender;
+		string btnPressed = button.Text;
+		operant = btnPressed;
+
+        if (negativeNumber == -1)
+        {
+            this.result.Text = "-";
+            negativeNumber = 1;
+        }
+        else
+		{
+            currentState = -2;
+            this.result.Text = "";
+        }
+
+        //history
+        currentOperationHistory = $"{firstNumber} {operant}";
+        UpdateHistory(sender, e, currentOperationHistory);
+    }
+
+    void OnCalculate(object sender, EventArgs e)
+	{
+		if (currentState == 2)
+		{
+			var result = Calculation.Calculate(operant, firstNumber, secondNumber);
+			this.result.Text = result.ToString();
+			
+			currentState = -1;
+
+			//history
+			currentOperationHistory = $"{firstNumber} {operant} {secondNumber}";
+            UpdateHistory(sender, e, currentOperationHistory);	
+
+            firstNumber = result;
+        }
+	}
+
+	void UpdateHistory(object sender, EventArgs e, string currentHistory)
+	{
+		this.currentOperationHistoryLabel.Text = currentHistory;
+
+		if (currentState ==-1)
+		{
+            history.Add(currentOperationHistory);
+            // currentOperationHistory = string.Empty;
+        }
+        else if(currentHistory=="")
+		{
+			history = new();
+            // currentOperationHistory = string.Empty;
+        }
+
+    }
+
+}
+
